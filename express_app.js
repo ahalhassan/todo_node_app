74f704f8b8f698949ce87a8869377f8daa5718d2
unknown <ahalhassa94@>
2021-12-21 15:13:26 -0800

    todo v4

--- express_app.js
+++ express_app.js
@@ -2,8 +2,7 @@ require('dotenv').config();
 const express = require("express");
 const mongoose = require("mongoose");
 const todoController = require('./controllers/todoController');
-
-
+const port = process.env.port || 4000;
 
 const server = express();
 
